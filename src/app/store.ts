10bc5fc8a108837dc227935869a01657cb79f340
alexdev93 <alemayehu.dev@gmail.com>
2024-09-06 20:25:12 +0300

    add the song saga middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,17 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import { all } from "redux-saga/effects";
 import exampleReducer from "../features/example/exampleSlice";
+import songReducer from "../features/songs/songSlice";
 import exampleSaga from "../features/example/exampleSaga";
+import songSaga from "../features/songs/songSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 function* rootSaga() {
-  yield all([exampleSaga()]);
+  yield all([exampleSaga(), songSaga()]);
 }
 
 const store = configureStore({
   reducer: {
     example: exampleReducer,
+    songs: songReducer,
   },
 
   middleware: (getDefaultMiddleware) =>
